926b52b0cee8395473fe30a84ab24b2e87987dbd
rutujabanswal <rutuja.banswal@gmail.com>
2017-09-21 23:33:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,8 @@
 console.log('Loaded!');
+var element = document.mainElementById('main-text');
+element.InnerHTML = 'new value';
+var = img document.mainElementById('img');
+img.onClick = function()
+{
+ img.style.MarginLeft='100px'
+};
